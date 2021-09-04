be9049d81ff82bce2094d29f09843fad566d4a19
OloloevReal <nickey.n@gmail.com>
2021-09-04 18:59:02 +0300

    lab_4_1 done

--- lab_4_1.cs
+++ lab_4_1.cs
@@ -1,11 +1,29 @@
 using System;
 using System.Text;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Lab_1_Part_4
 {
     class Lab_4_1: ILab{
+
+        private readonly string inputText = @"Порфирий Петрович несколько мгновений стоял, как бы вдумываясь, но вдруг опять вспорхнулся и замахал руками на непрошеных свидетелей. 
+Те мигом скрылись, и дверь притворилась. Затем он поглядел на стоявшего в углу Раскольникова, дико смотревшего на Николая, 
+и направился было к нему, но вдруг остановился, посмотрел на него, перевел тотчас же свой взгляд на Николая, 
+потом опять на Раскольникова, потом опять на Николая и вдруг, как бы увлеченный, опять набросился на Николая.";
+
+        private readonly string inputWord = "Николая";
+
         public override void Do(){
+            Console.WriteLine("Sourse text: ");
+            Console.WriteLine(inputText);
+            Console.WriteLine($"Entered word: {inputWord}");
+
+            Regex pat = new Regex(inputWord);
+            var ms = pat.Matches(inputText);
+            Console.WriteLine($"Found: {ms.Count}");
+
+            Console.WriteLine();       
         }
 
         public override string Description(){
