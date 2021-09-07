34cb28b7d76de03228a8915fb88631b53c4f7624
OloloevReal <nickey.n@gmail.com>
2021-09-07 20:01:26 +0300

    lab_4_9 done

--- lab_4_9.cs
+++ lab_4_9.cs
@@ -1,11 +1,19 @@
 using System;
-using System.Text;
-using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Lab_1_Part_4
 {
-    class Lab_4_9: ILab{
+    class Lab_4_9: ILab{       
+        private readonly string inputText = @"В эт0м предложении есть цифра и число 23. А в эт0м чисел нет.";
+        private readonly string pattern = @"(\d)+";
         public override void Do(){
+            Console.WriteLine("Entered text: ");
+            Console.WriteLine(inputText);
+
+
+            Console.WriteLine("Result: ");
+
+            Console.WriteLine(Regex.IsMatch(inputText, pattern)?"Yes!":"No!");
         }
 
         public override string Description(){
