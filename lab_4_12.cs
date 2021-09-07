e19d5d83487c154116a56d1cf0d2aa4f6a5b6fd5
OloloevReal <nickey.n@gmail.com>
2021-09-08 00:41:11 +0300

    lab_4_12, lab_4_13 done

--- lab_4_12.cs
+++ lab_4_12.cs
@@ -1,11 +1,22 @@
 using System;
-using System.Text;
-using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Lab_1_Part_4
 {
     class Lab_4_12: ILab{
+        private readonly string inputText = @"Утверждения - это проверки касательно символов, идущих до или после текущей позиции сопоставления, 
+ничего при этом не поглощая (никакие символы исходного текста не ставятся в соответствие утверждениям).";
+        private readonly string pattern = @"\b[а-яА-Я]+\b";
         public override void Do(){
+            Console.WriteLine("Entered text: ");
+            Console.WriteLine(inputText);
+
+            Console.WriteLine("Result: ");
+            foreach (var m in Regex.Matches(inputText, pattern)){
+                Console.WriteLine(m);
+            }
+
+            Console.WriteLine();
         }
 
         public override string Description(){
--- lab_4_13.cs
+++ lab_4_13.cs
@@ -1,11 +1,30 @@
 using System;
-using System.Text;
-using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Lab_1_Part_4
 {
     class Lab_4_13: ILab{
+        private readonly string inputText = @"Регулярное выражение - это шаблон, применяемый к заданному тексту слева направо.";
+        private readonly string pattern = @"(?i)[бвгджзлмнрхцчщпфктшс]";
+        /*
+        (?i) - ignore case and using unicode
+        */
         public override void Do(){
+            Console.WriteLine("Entered text: ");
+            Console.WriteLine(inputText);
+            Console.WriteLine($"Pattern: {pattern}\r\n");
+
+            var ms = Regex.Matches(inputText, pattern);
+            Console.WriteLine("Result: ");
+            Console.WriteLine($"Symbols: {ms.Count}\r\n");
+
+           foreach(var item in ms.GroupBy(x => x.Value).ToDictionary(x => x.Key, v => v.Count())){
+               Console.WriteLine($"Char: {item.Key} = {item.Value}");
+           }
+
+           Console.WriteLine();
+           
         }
 
         public override string Description(){
